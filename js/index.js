af252ad81e72fdfa3c18102a13861ef02f474af0
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-12-14 03:23:29 -0300

    novas paginas

--- js/index.js
+++ js/index.js
@@ -3,12 +3,6 @@ function loadPages(pages) {
     $(".body").load(`${pages}.html`)
 }
 
-$('.graficos').click(function(){
-    $(".content").addClass('d-none')
-    $(".sectionGraf").removeClass('d-none')
-})
-
-
 function search() {
     let pesquisa = $('.search').val()
     console.log(pesquisa)
